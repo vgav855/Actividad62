b3cd8ff6a217f828f56191cbd04eb51bd63110d5
vgav855 <132547267+vgav855@users.noreply.github.com>
2023-05-11 08:49:06 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let B = 0
+basic.forever(function () {
+    B = 255 - input.lightLevel()
+    led.setBrightness(B)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
